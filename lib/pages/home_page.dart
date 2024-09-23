cf76ffe2c2ede06c8e6a9f27f0fe684c13c23959
VeiTr-study <180214751+VeiTr-study@users.noreply.github.com>
2024-09-23 14:28:22 +0300

    init

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:market_place_flutter_e/pages/product_detail.dart';
+import '../models/product_model.dart';
+import '../components/item.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text('Marketplace', style: TextStyle(color: Colors.black))),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      ProductDetailPage(product: products[index]),
+                ),
+              );
+            },
+            child: Item(product: products[index]),
+          );
+        },
+      ),
+    );
+  }
+}
